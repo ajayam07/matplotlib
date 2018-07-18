122adef7ba80cd65ec2bc571bc78b7434b1a06e4
Ajay <ajayam07@hotmail.com>
2018-07-18 23:01:58 +0530

    Further lessons from Sentdex

--- intro.py
+++ intro.py
@@ -198,6 +198,80 @@ def graph_data(stock):
 graph_data('TSLA')
 
 
+### Basic Customization
+
+def graph_data(stock):
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((1,1), (0,0),)
+    # Unfortunately, Yahoo's API is no longer available
+    # feel free to adapt the code to another source, or use this drop-in replacement.
+    stock_price_url = 'https://pythonprogramming.net/yahoo_finance_replacement'
+
+    source_code = urllib.request.urlopen(stock_price_url).read().decode()
+
+    stock_data = []
+    split_source = source_code.split('\n')
+
+    for line in split_source[1:]:
+        split_line = line.split(',')
+        if len(split_line) == 7:
+            if 'values' not in line and 'labels' not in line:
+                stock_data.append(line)
+
+    date, closep, highp, lowp, openp, adj_closep, volume = np.loadtxt(stock_data,
+                                                                      delimiter=',',
+                                                                      unpack=True,
+                                                                      # %Y = full year. 2015
+                                                                      #  %y = partial year 15
+                                                                      #  %m = number month
+                                                                      #  %d = number day
+                                                                      #  %H = hours
+                                                                      #  %M = minutes
+                                                                      #  %S = seconds
+                                                                      #  12-06-2014
+                                                                      #  %m-%d-%Y
+                                                                      converters={0: bytespdate2num('%Y-%m-%d')})
+    ax1.plot_date(date, closep, '-', label='Price')
+    for label in ax1.xaxis.get_ticklabels():
+        label.set_rotation(45)
+    ax1.grid(True, color='g', linestyle='-', linewidth=0.5)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Int Graph\nCheck it out')
+    plt.legend()
+    plt.subplots_adjust(left=0.1, bottom=0.20,right=0.94, top=0.90,wspace=0.2, hspace=0)
+    plt.show()
+
+
+graph_data('TSLA')
+
+
+### Unix Time
+
+import datetime
+timestamp = 1339521878.04
+value = datetime.datetime.fromtimestamp(timestamp)
+print(value.strftime('%Y-%m-%d %H:%M:%S'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
