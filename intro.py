fe56db61510ce90165b42611227d13083b1fedfc
Ajay <ajayam07@hotmail.com>
2018-07-18 12:09:10 +0530

    Further lessons from Sentdex

--- intro.py
+++ intro.py
@@ -89,6 +89,120 @@ plt.show()
 
 ### Pie Charts
 
+# import matplotlib.pyplot as plt
+
+slices = [7,2,2,13]
+activities = ['sleeping','eating','working','playing']
+cols = ['c','m','r','b']
+
+plt.pie(slices,
+        labels=activities,
+        colors=cols,
+        startangle=90,
+        shadow= True,
+        explode=(0.1,0.1,0,0),
+        autopct='%1.1f%%')
+
+plt.title('Interesting Graph\nCheck it out')
+plt.show()
+
+
+### Loading Data from files
+### ver_1
+# import matplotlib.pyplot as plt
+import csv
+
+x = []
+y = []
+
+with open('example.txt','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+
+plt.plot(x,y, label='Loaded from file!')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
+
+
+### ver_2 (Faster, cleaner, better)
+# import matplotlib.pyplot as plt
+import numpy as np
+
+x, y = np.loadtxt('example.txt', delimiter=',', unpack=True)
+plt.plot(x,y, label='Loaded from file!')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
+
+### Data from Internet
+
+# import matplotlib.pyplot as plt
+import numpy as np
+import urllib
+import matplotlib.dates as mdates
+
+
+def bytespdate2num(fmt, encoding='utf-8'):
+    strconverter = mdates.strpdate2num(fmt)
+    def bytesconverter(b):
+        s = b.decode(encoding)
+        return strconverter(s)
+    return bytesconverter
+
+
+def graph_data(stock):
+    # Unfortunately, Yahoo's API is no longer available
+    # feel free to adapt the code to another source, or use this drop-in replacement.
+    stock_price_url = 'https://pythonprogramming.net/yahoo_finance_replacement'
+
+    source_code = urllib.request.urlopen(stock_price_url).read().decode()
+
+    stock_data = []
+    split_source = source_code.split('\n')
+
+    for line in split_source[1:]:
+        split_line = line.split(',')
+        if len(split_line) == 7:
+            if 'values' not in line and 'labels' not in line:
+                stock_data.append(line)
+
+    date, closep, highp, lowp, openp, adj_closep, volume = np.loadtxt(stock_data,
+                                                                      delimiter=',',
+                                                                      unpack=True,
+                                                                      # %Y = full year. 2015
+                                                                      #  %y = partial year 15
+                                                                      #  %m = number month
+                                                                      #  %d = number day
+                                                                      #  %H = hours
+                                                                      #  %M = minutes
+                                                                      #  %S = seconds
+                                                                      #  12-06-2014
+                                                                      #  %m-%d-%Y
+                                                                      converters={0: bytespdate2num('%Y-%m-%d')})
+    plt.plot_date(date, closep, '-', label='Price')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Int Graph\nCheck it out')
+    plt.legend()
+    plt.show()
+
+
+graph_data('TSLA')
+
+
+
+
+
+
+
 
 
 
