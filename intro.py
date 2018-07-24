4574a8cf60228cc70b33f8995d61ab8858a8bd20
Ajay <ajayam07@hotmail.com>
2018-07-24 14:14:08 +0530

    Bokeh basics added

--- intro.py
+++ intro.py
@@ -402,6 +402,7 @@ def graph_data(stock):
         label.set_rotation(45)
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y)%m-%d'))
     ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    ax1.grid(True)
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.title('EBAY Stocks')
@@ -414,15 +415,124 @@ graph_data('TWTR')
 
 
 
+### Styles with Matplotlib
+from matplotlib import style
+style.use('ggplot')
+style.use('fivethirtyeight')
+print(plt.style.available)
 
 
+### Live Graphs
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+style.use('dark_background')
+style.available
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
 
 
+def animate(i):
+    graph_data = open('example.txt','r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append(x)
+            ys.append(y)
+    ax1.clear()
+    ax1.plot(xs, ys)
 
 
+ani = animation.FuncAnimation(fig, animate, interval=3000)
+plt.show()
 
 
+### Annotations and Text with Matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
+from matplotlib.finance import candlestick_ohlc
+from matplotlib import style
 
+import numpy as np
+import urllib
+import datetime as dt
+
+style.use('fivethirtyeight')
+print(plt.style.available)
+
+print(plt.__file__)
+
+
+def bytespdate2num(fmt, encoding='utf-8'):
+    strconverter = mdates.strpdate2num(fmt)
+
+    def bytesconverter(b):
+        s = b.decode(encoding)
+        return strconverter(s)
+
+    return bytesconverter
+
+
+def graph_data(stock):
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((1, 1), (0, 0))
+
+    stock_price_url = 'https://pythonprogramming.net/yahoo_finance_replacement'
+    source_code = urllib.request.urlopen(stock_price_url).read().decode()
+    stock_data = []
+    split_source = source_code.split('\n')
+    for line in split_source:
+        split_line = line.split(',')
+        if len(split_line) == 6:
+            if 'values' not in line and 'labels' not in line:
+                stock_data.append(line)
+
+    date, closep, highp, lowp, openp, volume = np.loadtxt(stock_data,
+                                                          delimiter=',',
+                                                          unpack=True,
+                                                          converters={0: bytespdate2num('%Y%m%d')})
+
+    x = 0
+    y = len(date)
+    ohlc = []
+
+    while x < y:
+        append_me = date[x], openp[x], highp[x], lowp[x], closep[x], volume[x]
+        ohlc.append(append_me)
+        x += 1
+
+    candlestick_ohlc(ax1, ohlc, width=0.4, colorup='#77d879', colordown='#db3f3f')
+
+    for label in ax1.xaxis.get_ticklabels():
+        label.set_rotation(45)
+
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    ax1.grid(True)
+    ax1.annotate('Bad News!', (date[9], highp[9]),
+                 xytext=(0.8, 0.9), textcoords='axes fraction',
+                 arrowprops=dict(facecolor='grey', color='grey'))
+
+    ##    # Text placement example:
+    ##    font_dict = {'family':'serif',
+    ##                 'color':'darkred',
+    ##                 'size':15}
+    ##    ax1.text(date[10], closep[1],'Text Example', fontdict=font_dict)
+
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title(stock)
+    # plt.legend()
+    plt.subplots_adjust(left=0.09, bottom=0.20, right=0.94, top=0.90, wspace=0.2, hspace=0)
+    plt.show()
+
+
+graph_data('ebay')
 
 
 
